b668c3aa16344e7f6ad7f097bab033c732265757
debioliver <debi.oliver@hotmail.com>
2023-11-05 23:53:39 -0300

    Subindo exercícis de Javacript - 2023-11-05

---
+++ javascript/aula-1.js
@@ -0,0 +1,18 @@
+// Estudando variaveis
+
+const nome = "Deborah";
+let idade = 50;
+
+let numeroSoma = 5;
+let numeroSoma2 = 5;
+
+let soma = numeroSoma + numeroSoma2;
+
+
+
+console.log(soma)
+
+
+// Concatenando as variaveis
+
+// console.log("Olá, me chamo "+nome+", e tenho "+idade+" anos"); 
---
+++ javascript/aula-2.js
@@ -0,0 +1,31 @@
+//Verifcando o tamanho/comprimento de uma variavel
+
+const minhaVariavel = "aprendendo Javascript";
+const tamanhoVariavel = minhaVariavel.length;
+//console.log(tamanhoVariavel);
+
+//Substituindo valores de variaveis usando o .replace()
+
+let mudandoValores = 'SubstituindoJS'.replace('JS','Javascript');
+//console.log(mudandoValores);
+
+//Divide a string "dividindoString, utilizando como delimitador o i.
+let dividindoString ="dividindoString".split('i')
+//console.log(dividindoString);
+
+
+//Transforma um texto que está maisculo, para minusculo
+let minuscula = 'MINUSCULA'.toLowerCase();
+//console.log(minuscula);
+
+//Transforma um texto que está em minusculo, para maisculo
+let maiscula = 'maiscula'.toUpperCase();
+//console.log(maiscula);
+
+//Retorne uma substring/pedaço da minha variavel, começando em 0, e indo ate 11 no index.
+const frase = "Aprendendo JavaScript". substring(0, 11);
+//console.log(frase);
+
+//Retorne uma substring/pedaço da minha variavel, començando em 0 e indo até 2 no index.
+const frase2 = "Aprendendo JavaScript".substring(0, 2);
+//console.log(frase2);
